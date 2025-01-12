d0095f4cd14b1fa8c183b8b0c0b1de587f1e938f
Benjamin Wilkins <104269697+bwilkins96@users.noreply.github.com>
2025-01-12 09:07:39 -0500

    max heap

---
+++ max_heap.py
@@ -0,0 +1,19 @@
+import heapq
+
+class MaxHeap:
+    def __init__(self, data=[]):
+        data = [-x for x in data]
+        heapq.heapify(data)
+        self.data = data
+    
+    def push(self, ele):
+        heapq.heappush(self.data, -ele)
+    
+    def pop(self):
+        return -heapq.heappop(self.data)
+    
+    def __repr__(self):
+        return str(self.data)
+    
+    def __len__(self):
+        return len(self.data)
