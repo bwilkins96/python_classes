75e509eba37492b2bba5f37afdf7d3b1711b87f8
Benjamin Wilkins <104269697+bwilkins96@users.noreply.github.com>
2025-01-15 12:48:07 -0500

    stack

---
+++ stack.py
@@ -0,0 +1,18 @@
+class Stack:
+    def __init__(self, data=[]):
+        self.data = []
+    
+    def pop(self):
+        return self.data.pop()
+    
+    def peek(self):
+        return self.data[-1]
+    
+    def push(self, ele):
+        self.data.append(ele)
+    
+    def empty(self):
+        return len(self.data) == 0
+    
+    def __repr__(self):
+        return f'bottom - {self.data} - top'
